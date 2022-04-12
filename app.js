fc0855368b806605facb7b620431f6a68a568bfb
Mr.DxD <nguyendoai7777@gmail.com>
2022-04-12 21:51:51 +0700

    doing

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const toggle = document.querySelector('#toggle');
-let isBlock = true;
+let getItem = localStorage['fbBlockAdsState'] || 'yes';
+let isBlock;
+(function () {
+  isBlock = getItem === 'yes';
+  if(isBlock) {
+    toggle.classList.add('active');
+  } else {
+    toggle.classList.remove('active');
+  }
+})()
 toggle.addEventListener("click",  () => {
+  let clear;
   isBlock = !isBlock;
   if(isBlock) {
-    toggle.classList.add(['active'])
+    localStorage.setItem('fbBlockAdsState', 'yes');
+    toggle.classList.add('active');
+    const adv = document.querySelector('.gqgwhnc0.g2fypklz.pmk7jnqg');
+    clear = setInterval(() => {
+      if(adv) {
+        adv.remove();
+      }
+    }, 3000)
   } else {
-    toggle.classList.remove(['active'])
+    localStorage.setItem('fbBlockAdsState', 'no');
+    toggle.classList.remove('active');
+    clearInterval(clear);
   }
 });
--- background.js
+++ background.js
@@ -1,6 +1,4 @@
-let color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete')
+    chrome.tabs.executeScript(tabId, {file:"app.js"});
 });
